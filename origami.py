3195b46548156bffb299e3ab224d8f48946d5dbf
Bevan White <jefferybevan@gmail.com>
2022-09-27 09:34:24 +0530

    form creation,validation and styles are done

--- origami.py
+++ origami.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'eebb2c58cc974ff7833f182cc71dfd9e'
 
 origami = [
     {
@@ -26,6 +29,25 @@ def home():
 def about():
     return render_template('about.html', title='Cascade')
 
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. please check Username and Password')
+    return render_template('login.html',title='Login',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
