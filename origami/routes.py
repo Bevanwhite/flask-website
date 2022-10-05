193a2386cd5924c24813bc191634ba10eeceaaa3
Bevan White <jefferybevan@gmail.com>
2022-10-05 14:15:34 +0530

    added the curd functions for the post model

--- origami/routes.py
+++ origami/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from origami import app, db, bcrypt
 from origami.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from origami.models import User, Post
@@ -26,7 +26,6 @@ origami = [
 @app.route("/home")
 def home():
     origami = Post.query.all()
-    print(origami)
     return render_template('home.html', posts=origami)
 
 @app.route("/about")
@@ -112,4 +111,40 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', legend='New Post', form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update",methods=['GET','POST']) 
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = PostForm()
+    if post.author != current_user:
+        abort(403)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been update!', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content 
+    return render_template('create_post.html', title='Update Post', legend='Update Post' ,  form=form)
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
