50665b3cbe78063abb706f2ea85185c4cdf9e42b
Bevan White <jefferybevan@gmail.com>
2022-10-15 11:43:30 +0530

    added heroku postgres database

--- origami/routes.py
+++ origami/routes.py
@@ -148,8 +148,8 @@ def user_posts(username):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Password', sender='jeffery1996.jbw@gmail.com', recipients=[user.email])
-    msg.body = f''' To reset your password, vist the following link:
+    msg = Message('Password Reset From Origami', sender='jeffery1996.jbw@gmail.com', recipients=[user.email])
+    msg.body = f'''To reset your password, vist the following link:
 {url_for('reset_token', token=token, _external=True)}
 If you did not make this request then simply ignore this email and no changes will be done for the email password'
 '''
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from origami import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
